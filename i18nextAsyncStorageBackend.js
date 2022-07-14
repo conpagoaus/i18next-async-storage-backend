54df547fa0550639f1f0a2a66c3616d292827807
Emanuele Tonello <emanueletonello@gmail.com>
2022-07-14 16:16:47 +1000

    moved to new async storage package. updated entire build pipeline

--- i18nextAsyncStorageBackend.js
+++ i18nextAsyncStorageBackend.js
@@ -1,127 +1,139 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(global.i18nextAsyncStorageBackend = factory());
-}(this, (function () { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.i18nextAsyncStorageBackend = factory());
+})(this, (function () { 'use strict';
 
-var arr = [];
-var each = arr.forEach;
-var slice = arr.slice;
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
 
-function defaults(obj) {
-  each.call(slice.call(arguments, 1), function (source) {
-    if (source) {
-      for (var prop in source) {
-        if (obj[prop] === undefined) obj[prop] = source[prop];
+  function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    Object.defineProperty(Constructor, "prototype", {
+      writable: false
+    });
+    return Constructor;
+  }
+
+  var arr = [];
+  var each = arr.forEach;
+  var slice = arr.slice;
+  function defaults(obj) {
+    each.call(slice.call(arguments, 1), function (source) {
+      if (source) {
+        for (var prop in source) {
+          if (obj[prop] === undefined) obj[prop] = source[prop];
+        }
       }
-    }
-  });
-  return obj;
-}
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-// get from whatever version of react native that is being used.
-var AsyncStorage = require('@react-native-community/async-storage') || {};
-
-var storage = {
-  setItem: function setItem(key, value) {
-    if (AsyncStorage) {
-      return AsyncStorage.setItem(key, value);
-    }
-  },
-  getItem: function getItem(key, value) {
-    if (AsyncStorage) {
-      return AsyncStorage.getItem(key, value);
-    }
-    return undefined;
+    });
+    return obj;
   }
-};
 
-function getDefaults() {
-  return {
-    prefix: 'i18next_res_',
-    expirationTime: 7 * 24 * 60 * 60 * 1000,
-    versions: {}
+  var AsyncStorage = require('@react-native-async-storage/async-storage') || {};
+  var storage = {
+    setItem: function setItem(key, value) {
+      if (AsyncStorage) {
+        return AsyncStorage.setItem(key, value);
+      }
+    },
+    getItem: function getItem(key, value) {
+      if (AsyncStorage) {
+        return AsyncStorage.getItem(key, value);
+      }
+
+      return undefined;
+    }
   };
-}
 
-var Cache = function () {
-  function Cache(services) {
-    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-
-    _classCallCheck(this, Cache);
-
-    this.init(services, options);
-
-    this.type = 'backend';
+  function getDefaults() {
+    return {
+      prefix: 'i18next_res_',
+      expirationTime: 7 * 24 * 60 * 60 * 1000,
+      versions: {}
+    };
   }
 
-  _createClass(Cache, [{
-    key: 'init',
-    value: function init(services) {
+  var Cache = /*#__PURE__*/function () {
+    function Cache(services) {
       var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-      this.services = services;
-      this.options = defaults(options, this.options || {}, getDefaults());
+      _classCallCheck(this, Cache);
+
+      this.init(services, options);
+      this.type = 'backend';
     }
-  }, {
-    key: 'read',
-    value: function read(language, namespace, callback) {
-      var _this = this;
 
-      var store = {};
-      var nowMS = new Date().getTime();
-
-      if (!AsyncStorage) {
-        return callback(null, null);
+    _createClass(Cache, [{
+      key: "init",
+      value: function init(services) {
+        var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+        this.services = services;
+        this.options = defaults(options, this.options || {}, getDefaults());
       }
+    }, {
+      key: "read",
+      value: function read(language, namespace, callback) {
+        var _this = this;
+        var nowMS = new Date().getTime();
 
-      storage.getItem('' + this.options.prefix + language + '-' + namespace).then(function (local) {
-        if (local) {
-          local = JSON.parse(local);
-          if (
-          // expiration field is mandatory, and should not be expired
-          local.i18nStamp && local.i18nStamp + _this.options.expirationTime > nowMS &&
+        if (!AsyncStorage) {
+          return callback(null, null);
+        }
 
-          // there should be no language version set, or if it is, it should match the one in translation
-          _this.options.versions[language] === local.i18nVersion) {
-            delete local.i18nVersion;
-            delete local.i18nStamp;
-            return callback(null, local);
+        storage.getItem("".concat(this.options.prefix).concat(language, "-").concat(namespace)).then(function (local) {
+          if (local) {
+            local = JSON.parse(local);
+
+            if ( // expiration field is mandatory, and should not be expired
+            local.i18nStamp && local.i18nStamp + _this.options.expirationTime > nowMS && // there should be no language version set, or if it is, it should match the one in translation
+            _this.options.versions[language] === local.i18nVersion) {
+              delete local.i18nVersion;
+              delete local.i18nStamp;
+              return callback(null, local);
+            }
           }
-        }
 
-        callback(null, null);
-      }).catch(function (err) {
-        console.warn(err);
-        callback(null, null);
-      });
-    }
-  }, {
-    key: 'save',
-    value: function save(language, namespace, data) {
-      if (AsyncStorage) {
-        data.i18nStamp = new Date().getTime();
-
-        // language version (if set)
-        if (this.options.versions[language]) {
-          data.i18nVersion = this.options.versions[language];
-        }
-
-        // save
-        storage.setItem('' + this.options.prefix + language + '-' + namespace, JSON.stringify(data));
+          callback(null, null);
+        })["catch"](function (err) {
+          console.warn(err);
+          callback(null, null);
+        });
       }
-    }
-  }]);
+    }, {
+      key: "save",
+      value: function save(language, namespace, data) {
+        if (AsyncStorage) {
+          data.i18nStamp = new Date().getTime(); // language version (if set)
+
+          if (this.options.versions[language]) {
+            data.i18nVersion = this.options.versions[language];
+          } // save
+
+
+          storage.setItem("".concat(this.options.prefix).concat(language, "-").concat(namespace), JSON.stringify(data));
+        }
+      }
+    }]);
+
+    return Cache;
+  }();
+
+  Cache.type = 'backend';
 
   return Cache;
-}();
 
-Cache.type = 'backend';
-
-return Cache;
-
-})));
+}));
--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,55 @@
-import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
+import {
+  babel
+} from '@rollup/plugin-babel';
+import uglify from "@lopatnov/rollup-plugin-uglify";
 import nodeResolve from 'rollup-plugin-node-resolve';
-import { argv } from 'yargs';
+import yargs from 'yargs'
+import {
+  hideBin
+} from 'yargs/helpers'
+
+const argv = yargs(hideBin(process.argv)).argv
 
 const format = argv.format || argv.f || 'iife';
-const compress = argv.uglify;
+const compress = argv.compact;
 
 const babelOptions = {
   exclude: 'node_modules/**',
-  presets: [['es2015', { modules: false }], 'stage-0'],
-  babelrc: false
+  presets: [
+    ["@babel/preset-env", {
+      modules: false
+    }]
+  ],
+  babelrc: false,
+  babelHelpers: 'bundled'
 };
 
-const dest = {
-  amd: `dist/amd/i18nextAsyncStorageBackend${compress ? '.min' : ''}.js`,
-  umd: `dist/umd/i18nextAsyncStorageBackend${compress ? '.min' : ''}.js`,
-  iife: `dist/iife/i18nextAsyncStorageBackend${compress ? '.min' : ''}.js`
-}[format];
+const moduleName = 'i18nextAsyncStorageBackend';
+
+const output = [{
+    file: `dist/amd/i18nextAsyncStorageBackend${compress ? '.min' : ''}.js`,
+    name: moduleName,
+    format: 'amd'
+  },
+  {
+    file: `dist/umd/i18nextAsyncStorageBackend${compress ? '.min' : ''}.js`,
+    name: moduleName,
+    format: 'umd'
+  },
+  {
+    file: `dist/iife/i18nextAsyncStorageBackend${compress ? '.min' : ''}.js`,
+    name: moduleName,
+    format: 'iife'
+  },
+];
 
 export default {
-  entry: 'src/index.js',
-  format,
+  input: 'src/index.js',
   plugins: [
     babel(babelOptions),
-    nodeResolve({ jsnext: true })
+    nodeResolve({
+      jsnext: true
+    })
   ].concat(compress ? uglify() : []),
-  moduleName: 'i18nextAsyncStorageBackend',
-  // moduleId: 'i18nextAsyncStorageCache',
-  dest
+  output
 };
